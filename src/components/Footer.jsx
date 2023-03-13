12abb95c69daab1a496c56ffba44d2cea3e9026a
Teppei-tt <teppei.takata03@gmail.com>
2023-03-13 15:19:50 +0900

    Tailwindの拡張機能追加テスト

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const Footer = () => {
     return (
-        <footer className="text-center bg-white py-10 text-sm">
+        <footer className="text-center bg-white text-sm py-11">
             <p>©️KAKKOEASY</p>
         </footer>
     );
